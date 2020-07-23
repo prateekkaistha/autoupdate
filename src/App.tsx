f7f1a612aa9e0a073bac03d9f7bde0b801151c4b
Prateek <kaistha.prateek03@gmail.com>
2020-07-23 11:33:21 +0530

    release testing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editing again last time <code>src/App.tsx</code> and save to reload.
+          Editing again last time testing without code upload <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
